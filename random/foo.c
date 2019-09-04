674e249eb7a8936e5011a521f3d228a2a5725e66
andrew <mrju.email@gmail.com>
2019-09-04 11:35:35 +0800

    random generating
    Signed-off-by: andrew <mrju.email@gmail.com>

---
+++ random/foo.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 Andrew <mrju.email@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/random.h>
+
+#define STR(x) _STR(x)
+#define _STR(x) #x
+
+#define VERSION_PREFIX Foo
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 0
+#define PATCH_VERSION 0
+
+#define VERSION STR(VERSION_PREFIX-MAJOR_VERSION.MINOR_VERSION.PATCH_VERSION)
+
+static int __init foo_init(void)
+{
+	unsigned long rand[16];
+	int i;
+
+	for (i = 0; i < 16; i++) {
+		get_random_bytes(&rand[i], sizeof(unsigned long));
+		printk("%s %s %d %ld\n",
+			__FILE__, __func__, __LINE__, rand[i]);
+	}
+
+	return 0;
+}
+
+static void __exit foo_exit(void)
+{
+}
+
+module_init(foo_init);
+module_exit(foo_exit);
+
+MODULE_ALIAS("foo-driver");
+MODULE_LICENSE("GPL");
+MODULE_VERSION(VERSION);
+MODULE_DESCRIPTION("Linux is not Unix");
+MODULE_AUTHOR("andrew, mrju.email@gmail.com");
